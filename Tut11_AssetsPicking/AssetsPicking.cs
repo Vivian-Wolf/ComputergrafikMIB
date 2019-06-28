ef40fbfce69350879dcfd92c93dc55808742f534
Vivian Wolf <vivian.wolf@outlook.de>
2019-06-28 19:25:55 +0200

    Vollständig

--- Tut11_AssetsPicking/AssetsPicking.cs
+++ Tut11_AssetsPicking/AssetsPicking.cs
@@ -18,67 +18,152 @@ namespace Fusee.Tutorial.Core
     {
         private SceneContainer _scene;
         private SceneRenderer _sceneRenderer;
-        private TransformComponent _baseTransform;
-
-        SceneContainer CreateScene()
-        {
-            // Initialize transform components that need to be changed inside "RenderAFrame"
-            _baseTransform = new TransformComponent
-            {
-                Rotation = new float3(0, 0, 0),
-                Scale = new float3(1, 1, 1),
-                Translation = new float3(0, 0, 0)
-            };
-
-            // Setup the scene graph
-            return new SceneContainer
-            {
-                Children = new List<SceneNodeContainer>
-                {
-                    new SceneNodeContainer
-                    {
-                        Components = new List<SceneComponentContainer>
-                        {
-                            // TRANSFROM COMPONENT
-                            _baseTransform,
-
-                            // SHADER EFFECT COMPONENT
-                            new ShaderEffectComponent
-                            {
-                                Effect = SimpleMeshes.MakeShaderEffect(new float3(0.7f, 0.7f, 0.7f), new float3(1, 1, 1), 5)
-                            },
-
-                            // MESH COMPONENT
-                            // SimpleAssetsPickinges.CreateCuboid(new float3(10, 10, 10))
-                            SimpleMeshes.CreateCuboid(new float3(10, 10, 10))
-                        }
-                    },
-                }
-            };
-        }
+        private ScenePicker _scenePicker;
+        private TransformComponent _leiterBack;
+        private TransformComponent _leiterFront;
+        private TransformComponent _DS;
+        private ShaderEffectComponent _leiterBackShader;
+        private ShaderEffectComponent _leiterFrontShader;
+        private ShaderEffectComponent _DSShader;
+        private PickResult _currentPick;
+        private float3 _oldColor;
+        private float3 _newColor;
+        private float3 _oldColorLB;
+        private float3 _oldColorLF;
+        private float3 _oldColorDS;
+        private float leiterBackRot;
 
         // Init is called on startup. 
         public override void Init()
         {
             // Set the clear color for the backbuffer to white (100% intensity in all color channels R, G, B, A).
-            RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
+            RC.ClearColor = new float4(0.2f, 0.3f, 1.0f, 1);
 
-            _scene = CreateScene();
+            _scene = AssetStorage.Get<SceneContainer>("fahrzeug.fus");
 
             // Create a scene renderer holding the scene above
             _sceneRenderer = new SceneRenderer(_scene);
+            _scenePicker = new ScenePicker(_scene);
         }
 
         // RenderAFrame is called once a frame
         public override void RenderAFrame()
         {
-            _baseTransform.Rotation = new float3(0, M.MinAngle(TimeSinceStart), 0);
+            // _baseTransform.Rotation = new float3(0, M.MinAngle(TimeSinceStart), 0);
+
+            //Rotation Drehsteuerung
+            _DSShader = _scene.Children.FindNodes(node => node.Name == "Drehsteuerung")?.FirstOrDefault()?.GetComponent<ShaderEffectComponent>();
+            _oldColorDS = (float3)_DSShader.Effect.GetEffectParam("DiffuseColor");
+            if (_oldColorDS == _newColor)
+            {
+
+                _DS = _scene.Children.FindNodes(node => node.Name == "Drehsteuerung")?.FirstOrDefault()?.GetTransform();
+                float DSRot = _DS.Rotation.y;
+                DSRot += 0.4f * Keyboard.ADAxis * Time.DeltaTime;
+                _DS.Rotation = new float3(0, DSRot, 0);
+
+            }
+
+            //Rotation hintere Leiter
+            _leiterBackShader = _scene.Children.FindNodes(node => node.Name == "LeiterHinten")?.FirstOrDefault()?.GetComponent<ShaderEffectComponent>();
+            _oldColorLB = (float3)_leiterBackShader.Effect.GetEffectParam("DiffuseColor");
+            if (_oldColorLB == _newColor)
+            {
+
+                _leiterBack = _scene.Children.FindNodes(node => node.Name == "LeiterHinten")?.FirstOrDefault()?.GetTransform();
+                leiterBackRot = _leiterBack.Rotation.x;
+                leiterBackRot += 0.4f * Keyboard.WSAxis * Time.DeltaTime;
+
+                if (leiterBackRot <= 0)
+                {
+                    _leiterBack.Rotation = new float3(0, 0, 0);
+                }
+
+                else if (leiterBackRot <= 60 * M.Pi / 180)
+                {
+                    _leiterBack.Rotation = new float3(leiterBackRot, 0, 0);
+                }
+
+                else if (leiterBackRot >= 60 * M.Pi / 180)
+                {
+                    leiterBackRot = 60 * M.Pi / 180;
+                    _leiterBack.Rotation = new float3(leiterBackRot, 0, 0);
+                }
+            }
+
+            //Rotation vordere Leiter
+            _leiterFrontShader = _scene.Children.FindNodes(node => node.Name == "LeiterVorne")?.FirstOrDefault()?.GetComponent<ShaderEffectComponent>();
+            _oldColorLF = (float3)_leiterFrontShader.Effect.GetEffectParam("DiffuseColor");
+            if (_oldColorLF == _newColor)
+            {
+                _leiterFront = _scene.Children.FindNodes(node => node.Name == "LeiterVorne")?.FirstOrDefault()?.GetTransform();
+                float leiterFrontRot = _leiterFront.Rotation.x;
+                leiterFrontRot += 0.4f * Keyboard.WSAxis * Time.DeltaTime;
+
+                if (leiterBackRot == 0)
+                {
+                    if (leiterFrontRot <= 0)
+                    {
+                        _leiterFront.Rotation = new float3(0, 0, 0);
+                    }
+                }
+
+                else if (leiterFrontRot <= -30 * M.Pi / 180)
+                {
+                    leiterFrontRot = -30 * M.Pi / 180;
+                    _leiterFront.Rotation = new float3(leiterFrontRot, 0, 0);
+                }
+
+                else if (leiterFrontRot <= 30 * M.Pi / 180)
+                {
+                    _leiterFront.Rotation = new float3(leiterFrontRot, 0, 0);
+                }
+
+                else if (leiterFrontRot >= 30 * M.Pi / 180)
+                {
+                    leiterFrontRot = 30 * M.Pi / 180;
+                    _leiterFront.Rotation = new float3(leiterFrontRot, 0, 0);
+                }
+            }
 
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
             // Setup the camera 
-            RC.View = float4x4.CreateTranslation(0, 0, 40) * float4x4.CreateRotationX(-(float) Atan(15.0 / 40.0));
+            RC.View = float4x4.CreateTranslation(0, 0, 20) * float4x4.CreateRotationX(-(float)Atan(10.0 / 40.0)) * float4x4.CreateRotationY(-(float)Acos(0));
+
+            //Scene Picker
+            if (Mouse.LeftButton)
+            {
+                float2 pickPosClip = Mouse.Position * new float2(2.0f / Width, -2.0f / Height) + new float2(-1, 1);
+                _scenePicker.View = RC.View;
+                _scenePicker.Projection = RC.Projection;
+
+                List<PickResult> pickResults = _scenePicker.Pick(pickPosClip).ToList();
+                PickResult newPick = null;
+                if (pickResults.Count > 0)
+                {
+                    pickResults.Sort((a, b) => Sign(a.ClipPos.z - b.ClipPos.z));
+                    newPick = pickResults[0];
+                }
+
+                if (newPick?.Node != _currentPick?.Node)
+                {
+                    if (_currentPick != null)
+                    {
+                        ShaderEffectComponent shaderEffectComponent = _currentPick.Node.GetComponent<ShaderEffectComponent>();
+                        shaderEffectComponent.Effect.SetEffectParam("DiffuseColor", _oldColor);
+                    }
+                    if (newPick != null)
+                    {
+                        ShaderEffectComponent shaderEffectComponent = newPick.Node.GetComponent<ShaderEffectComponent>();
+                        _oldColor = (float3)shaderEffectComponent.Effect.GetEffectParam("DiffuseColor");
+                        shaderEffectComponent.Effect.SetEffectParam("DiffuseColor", new float3(1, 0.19f, 0.29f));
+                        _newColor = (float3)shaderEffectComponent.Effect.GetEffectParam("DiffuseColor");
+                    }
+                    _currentPick = newPick;
+                }
+            }
 
             // Render the scene on the current render context
             _sceneRenderer.Render(RC);
